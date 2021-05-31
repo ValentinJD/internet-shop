ad04fd691fb047b820498d5981d4e5c44eeb716c
Валентин <moysait1984@yandex.ru>
2021-05-31 21:31:06 +0400

    add_DeliveryCompany_Fields

--- src/main/java/ru/internet/shop/model/DeliveryCompany.java
+++ src/main/java/ru/internet/shop/model/DeliveryCompany.java
@@ -1,4 +1,43 @@
 package ru.internet.shop.model;
 
-public class DeliveryCompany {
+public class DeliveryCompany extends AbstractModel{
+    private String name;
+
+    public DeliveryCompany(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        DeliveryCompany that = (DeliveryCompany) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryCompany{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
