28fa5b503777db87f77adc4eb64742fb2baa53af
Валентин <moysait1984@yandex.ru>
2021-05-31 20:05:15 +0400

    add_AbstractModel

---
+++ src/main/java/ru/internet/shop/model/AbstractModel.java
@@ -0,0 +1,38 @@
+package ru.internet.shop.model;
+
+import java.util.Objects;
+
+public abstract class AbstractModel {
+
+    protected Integer id;
+
+
+    public AbstractModel() {
+    }
+
+    public boolean isNew() {
+        return this.id == null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractModel that = (AbstractModel) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
