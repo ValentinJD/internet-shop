21f956ae01c419c124adf9ce5b9da622d3907632
Валентин <moysait1984@yandex.ru>
2021-05-31 20:38:12 +0400

    add_User_fields

--- src/main/java/ru/internet/shop/model/User.java
+++ src/main/java/ru/internet/shop/model/User.java
@@ -1,4 +1,197 @@
 package ru.internet.shop.model;
 
-public class User {
+import java.util.Objects;
+import java.util.Set;
+
+public class User extends AbstractModel {
+
+    private Order order;
+
+    private Basket basket;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String login;
+
+    private String password;
+
+    private Language language;
+
+    private Status status;
+
+    private Set<Role> roles;
+
+    private String telephone;
+
+    private String email;
+
+    private Integer discount;
+
+    private Integer amount;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Basket basket) {
+        this.basket = basket;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        User user = (User) o;
+
+        if (!discount.equals(user.discount)) return false;
+        if (!amount.equals(user.amount)) return false;
+        if (!Objects.equals(order, user.order)) return false;
+        if (!Objects.equals(basket, user.basket)) return false;
+        if (!Objects.equals(firstName, user.firstName)) return false;
+        if (!Objects.equals(lastName, user.lastName)) return false;
+        if (!Objects.equals(login, user.login)) return false;
+        if (!Objects.equals(password, user.password)) return false;
+        if (!Objects.equals(language, user.language)) return false;
+        if (status != user.status) return false;
+        if (!Objects.equals(roles, user.roles)) return false;
+        if (!Objects.equals(telephone, user.telephone)) return false;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (order != null ? order.hashCode() : 0);
+        result = 31 * result + (basket != null ? basket.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (language != null ? language.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (roles != null ? roles.hashCode() : 0);
+        result = 31 * result + (telephone != null ? telephone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + discount;
+        result = 31 * result + amount;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "order=" + order +
+                ", basket=" + basket +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", login='" + login + '\'' +
+                ", language=" + language +
+                ", status=" + status +
+                ", roles=" + roles +
+                ", telephone='" + telephone + '\'' +
+                ", email='" + email + '\'' +
+                ", discount=" + discount +
+                ", amount=" + amount +
+                '}';
+    }
 }
