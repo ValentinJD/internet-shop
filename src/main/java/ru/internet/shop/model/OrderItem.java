ed58418a39efdba070c05ce041bf0d1ce17538ba
Валентин <moysait1984@yandex.ru>
2021-05-31 20:55:28 +0400

    add_OrderItem

---
+++ src/main/java/ru/internet/shop/model/OrderItem.java
@@ -0,0 +1,13 @@
+package ru.internet.shop.model;
+
+
+import java.util.List;
+
+public class OrderItem extends AbstractModel{
+
+    private List<Product> productList;
+
+    private Integer countProduct;
+
+
+}
