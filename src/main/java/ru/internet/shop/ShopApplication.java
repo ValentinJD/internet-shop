fa6e0bc58eeeee8d04bd84f5984e973fd815f661
Валентин <moysait1984@yandex.ru>
2021-06-15 23:09:44 +0400

    add_UserRepository

--- src/main/java/ru/internet/shop/ShopApplication.java
+++ src/main/java/ru/internet/shop/ShopApplication.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import ru.internet.shop.model.User;
 import ru.internet.shop.repository.UserRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ShopApplication {
@@ -22,7 +25,34 @@ public class ShopApplication {
 //        System.out.println("Бины:");
 //        Arrays.stream(MyTestContext.getContext().getBeanDefinitionNames()).forEach(System.out::println);
 
-        UserRepository repository = context.getBean(UserRepository.class);
-        System.out.println("Список юзеров: \n" + repository.findAll());
+        // Тестируем UserRepository
+//        UserRepository repository = context.getBean(UserRepository.class);
+//
+//        System.out.println("Список юзеров: \n" + repository.findAll());
+//
+//        User user = new User("ФестНейм", "ЛастНейм", "login", "pass", "911",
+//                "a@mmm.com");
+//
+//
+//        User userDB = repository.save(user);
+//        System.out.println("Юзер из бд: " + userDB);
+//
+//
+//        List<User> userList = repository.findAll();
+//        System.out.println("Лист с новым юзером \n");
+//        userList.forEach((System.out::println));
+//
+//        System.out.printf("Удаляем %s \n", userDB.getId());
+//        repository.deleteById(userDB.getId());
+//
+//        System.out.println("Лист без нового юзера: \n");
+//        repository.findAll().forEach((System.out::println));
+//
+//        User userAdmin = repository.findById(100000).get();
+//
+//        System.out.println("Получаем Юзера по id бд: " + userAdmin);
+
     }
+
+
 }
--- src/main/java/ru/internet/shop/model/User.java
+++ src/main/java/ru/internet/shop/model/User.java
@@ -3,10 +3,11 @@ package ru.internet.shop.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Entity
 @Table(name = "users")
-public class User extends AbstractModel  {
+public class User extends AbstractModel {
 
     @Column(name = "first_name", nullable = false)
     private String firstName;
@@ -32,6 +33,33 @@ public class User extends AbstractModel  {
     @Column(name = "discount", nullable = false)
     private Float discount;
 
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String login, String password, String telephone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.password = password;
+        this.telephone = telephone;
+        this.email = email;
+        this.amount = 0f;
+        this.discount = 0f;
+    }
+
+    public User(Integer id, String firstName, String lastName, String login, String password, String telephone,
+                String email ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.password = password;
+        this.telephone = telephone;
+        this.email = email;
+        this.amount = 0f;
+        this.discount = 0f;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -45,5 +73,35 @@ public class User extends AbstractModel  {
                 ", amount=" + amount +
                 ", discount=" + discount +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (!Objects.equals(firstName, user.firstName)) return false;
+        if (!Objects.equals(lastName, user.lastName)) return false;
+        if (!Objects.equals(login, user.login)) return false;
+        if (!Objects.equals(password, user.password)) return false;
+        if (!Objects.equals(telephone, user.telephone)) return false;
+        if (!Objects.equals(email, user.email)) return false;
+        if (!Objects.equals(amount, user.amount)) return false;
+        return Objects.equals(discount, user.discount);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (login != null ? login.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (telephone != null ? telephone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (amount != null ? amount.hashCode() : 0);
+        result = 31 * result + (discount != null ? discount.hashCode() : 0);
+        return result;
     }
 }
